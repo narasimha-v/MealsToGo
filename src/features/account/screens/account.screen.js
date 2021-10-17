8f0c29f76e8b68ccefc6cddc092b48f5b108a471
Narasimha <vnarasimha.connect@gmail.com>
2021-10-17 19:49:32 +0530

    Welcome screen setup

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,4 +1,33 @@
 import React from 'react';
-import { AccountBackground } from '../components';
+import { Spacer } from '../../../components/spacer';
+import {
+	AccountBackground,
+	AccountContainer,
+	AccountCover,
+	AuthButton
+} from '../components';
 
-export const AccountScreen = () => <AccountBackground />;
+export const AccountScreen = ({ navigation }) => (
+	<AccountBackground>
+		<AccountCover />
+		<AccountContainer>
+			<AuthButton
+				icon={'lock-open-outline'}
+				mode={'contained'}
+				onPress={() => {
+					navigation.navigate('Login');
+				}}>
+				Login
+			</AuthButton>
+			<Spacer size={'large'} />
+			<AuthButton
+				icon={'email'}
+				mode={'contained'}
+				onPress={() => {
+					navigation.navigate('Register');
+				}}>
+				Register
+			</AuthButton>
+		</AccountContainer>
+	</AccountBackground>
+);
